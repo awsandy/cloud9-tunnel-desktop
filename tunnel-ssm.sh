508c21c9f93e49962ead36e300f5b42da6236110
awsandy <awsandy@amazon.com>
2022-02-17 11:27:17 +0000

    fix tunnel

--- tunnel-ssm.sh
+++ tunnel-ssm.sh
@@ -1,14 +1,14 @@
 if [[ "$1" == "" ]];then
-	echo "must pass name from EC2 Name tag or insyance id" && exit
+	echo "must pass name from EC2 Name tag or instance id" && exit
 fi
 # find the instance ID based on Tag Name
 if [[ $1 == "i-"* ]];then
-INSTANCE_ID=$(aws --profile rds --region eu-west-2 ec2 describe-instances \
+INSTANCE_ID=$(aws ec2 describe-instances \
                --filter "Name=tag:Name,Values=$1" \
                --query "Reservations[].Instances[?State.Name == 'running'].InstanceId[]" \
                --output text)
 else
-INSTANCE_ID=$(aws --profile rds --region eu-west-2 ec2 describe-instances \
+INSTANCE_ID=$(aws ec2 describe-instances \
                --instance-id $1 \
                --query "Reservations[].Instances[?State.Name == 'running'].InstanceId[]" \
                --output text)
@@ -17,7 +17,7 @@ fi
 if [[ "$INSTANCE_ID" == "i-"* ]]; then
 echo "Instance=$INSTANCE_ID"
 # create the port forwarding tunnel
-aws --profile rds --region eu-west-2 ssm start-session --target $INSTANCE_ID \
+aws ssm start-session --target $INSTANCE_ID \
                        --document-name AWS-StartPortForwardingSession \
                        --parameters '{"portNumber":["3389"],"localPortNumber":["9999"]}' &
 echo "Use localhost:9999"
